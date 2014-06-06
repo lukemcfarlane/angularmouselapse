23e39c5c4f8fd30b956175eed3b03ab5689e6703
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-06-06 20:57:29 +1200

    Initial commit

---
+++ app/controllers/mainCtrl.js
@@ -0,0 +1,19 @@
+/**
+ * This is the main controller for the app, used in index.html.
+ *
+ * @author  AUTHOR
+ * @date    DATE
+ */
+app.controller('mainCtrl', function($scope) {
+	$scope.images = [
+		'http://metservice.com/IcePics/ob/249a-1467013bc00-1467016a2c9.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-146701a24a0-146701dcee6.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-146702177a0-1467024996b.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-1467027e040-146702b2949.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-146702f3340-14670325565.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-14670359be0-14670391fea.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-146703ceee0-146703ffde7.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-14670435780-1467046dbe5.ImgPlayerCHRISTCHURCH.jpeg',
+		'http://metservice.com/IcePics/ob/249a-146704aaa80-146704df433.ImgPlayerCHRISTCHURCH.jpeg'
+	];
+});
---
+++ app/directives/myDirective.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * @author  AUTHOR
+ * @date    DATE
+ */
+app.directive('mouseLapse', function() {
+	var makeActive = function(index, arr) {
+		for (var i = 0; i < arr.length; i++) {
+			arr[i].active = false;
+		}
+		arr[index].active = true;
+	};
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			images: '='
+		},
+		controller: function($scope, $element, $window) {
+			$scope.imageDataArr = [];
+			for (var i = 0; i < $scope.images.length; i++) {
+				$scope.imageDataArr.push({
+					index: i,
+					url: $scope.images[i],
+					active: false
+				});
+			}
+
+			makeActive(0, $scope.imageDataArr);
+
+			var windowWidth = $window.innerWidth;
+			var numCols = $scope.imageDataArr.length;
+
+			$element.on('mousemove', function($e) {
+				var mouseX = $e.clientX;
+				var col = Math.floor((mouseX / windowWidth) * numCols);
+				makeActive(col, $scope.imageDataArr);
+				$scope.$apply();
+			});
+		},
+		template: '<div class="ml-container" >' +
+			'<div class="ml-image-container">' +
+			'<img ng-repeat="img in imageDataArr" ng-src="{{ img.url }}" ng-show="img.active"></img>' +
+			'</div>' +
+			'</div>'
+	};
+});
